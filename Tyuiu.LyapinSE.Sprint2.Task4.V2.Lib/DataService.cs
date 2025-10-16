609da76ea5a6e992ace7bc77263c5aec7a4da5da
Zeron <dotdot20072607@gmail.com>
2025-10-16 23:18:16 +0500

    fix: Task4

--- Tyuiu.LyapinSE.Sprint2.Task4.V2.Lib/DataService.cs
+++ Tyuiu.LyapinSE.Sprint2.Task4.V2.Lib/DataService.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.LyapinSE.Sprint2.Task4.V2.Lib
     {
         public double Calculate(double x, double y)
         {
-            double z = (x > y + 3) ? x + (x + 1 / y) : ((y + 5) / (y + 2));
+            double z = x > (y + 3) ? x + (x / y) : (y + 5) / (y + 2);
             return Math.Round(z, 3);
         }
     }
--- Tyuiu.LyapinSE.Sprint2.Task4.V2.Test/DataServiceTest.cs
+++ Tyuiu.LyapinSE.Sprint2.Task4.V2.Test/DataServiceTest.cs
@@ -1,4 +1,6 @@
-﻿namespace Tyuiu.LyapinSE.Sprint2.Task4.V2.Test
+﻿using Tyuiu.LyapinSE.Sprint2.Task4.V2.Lib;
+
+namespace Tyuiu.LyapinSE.Sprint2.Task4.V2.Test
 {
     [TestClass]
     public sealed class DataServiceTest
@@ -6,6 +8,12 @@
         [TestMethod]
         public void TestMethod1()
         {
+            DataService ds = new DataService();
+
+            double x = 9;
+            double y = 4;
+
+            Assert.AreEqual(11.5, ds.Calculate(x, y));
         }
     }
 }
